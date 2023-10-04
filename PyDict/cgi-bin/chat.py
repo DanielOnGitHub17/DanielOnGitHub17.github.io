792aa261d897a3c0b63a7019943276c98440beef
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2023-10-04 00:02:22 -0500

    Delete PyDict directory

--- PyDict/cgi-bin/chat.py
+++
@@ -1,6 +0,0 @@
-import cgi, cgitb, os, sys
-P = print
-x = cgi.FieldStorage().getfirst("name")
-P("Content-type: html")
-P()
-P(f"Hello {x}!")
--- PyDict/chat.js
+++
@@ -1,7 +0,0 @@
-let savar = get("savar"),
-  m = get("messageBox");
-savar.onclick = () =>{
-  fetch(`cgi-bin/chat.py?name=${messageBox.value}`).then(i=>{
-    i.text().then(j=>alert(j))
-  })
-}
