a423475741eb9ec06f2573d9113031ba97bbd217
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-03-11 11:29:25 -0500

    Update localhost.py
    Good practice

--- PhonePatterns/localhost.py
+++ PhonePatterns/localhost.py
@@ -1,5 +1,5 @@
 from http.server import *
-def startHost(port=8000, path='', server_class=HTTPServer, handler_class=CGIHTTPRequestHandler):
+def start_host(port=8000, path='', server_class=HTTPServer, handler_class=CGIHTTPRequestHandler):
     server_address = (path, port)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
@@ -7,5 +7,5 @@ def startHost(port=8000, path='', server_class=HTTPServer, handler_class=CGIHTTP
 import sys
 if __name__=='__main__':
     args = sys.argv
-    startHost(int(args[1]) if len(args)-1 else int(input("port: ")),
+    start_host(int(args[1]) if len(args)-1 else int(input("port: ")),
               args[2] if len(args)>2 else '')
